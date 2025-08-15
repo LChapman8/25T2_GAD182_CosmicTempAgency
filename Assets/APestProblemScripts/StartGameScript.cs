b101aa8184af5f95e693a786ec5f84884db9c769
Siauosi <1033918@student.sae.edu.au>
2025-08-15 14:44:02 +1000

    Implemented compatibility for end of A Pest Problem game and main menu. Increased playerspeed within main menu, to make testing more efficient.

--- Assets/APestProblemScripts/StartGameScript.cs
+++ Assets/APestProblemScripts/StartGameScript.cs
@@ -10,6 +10,8 @@ public class StartGameScript : MonoBehaviour
     void Start()
     {
         Time.timeScale = 0f;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
     }
 
     // Update is called once per frame
